cda144fae3f457819e8292daf3d4b00f64bf17e6
Thomas Weidner <tomweidner103@gmail.com>
2019-01-04 16:16:54 -0800

    working ok, landing is spotty

--- app/main.js
+++ app/main.js
@@ -1,21 +1,50 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React from "react";
+import ReactDOM from "react-dom";
+import Axios from "axios";
 
 export default class Main extends React.Component {
-    constructor(){
-        super()
-    }
-    render(){
-        return(
-            <div>
-            <div>Hello</div>
-            </div>
+  constructor() {
+    super();
+    this.fly = this.fly.bind(this);
+  }
+  fly(name) {
+    Axios.get(`/api/drone/${name}`);
+  }
 
-        )
-    }
+  render() {
+    return (
+      <div>
+        <button
+          onClick={() => {
+            this.fly("takeoff");
+          }}
+        >
+          takeoff
+        </button>
+        <button
+          onClick={() => {
+            this.fly("land");
+          }}
+        >
+          land
+        </button>
+        <button
+          onClick={() => {
+            this.fly("reset");
+          }}
+        >
+          reset
+        </button>
+        <button
+          onClick={() => {
+            this.fly("battery");
+          }}
+        >
+          battery
+        </button>
+      </div>
+    );
+  }
 }
 
-ReactDOM.render(
-        <Main />,
-    document.getElementById('app')
-)
+ReactDOM.render(<Main />, document.getElementById("app"));
--- server/api/drone.js
+++ server/api/drone.js
@@ -1,18 +1,37 @@
 const router = require('express').Router()
 const arDrone = require('ar-drone')
+const parrot = require('node-parrot-drone')
+const drone = new parrot.Wifi
 const client = arDrone.createClient()
 client.createRepl()
 
+const connected = () => {
+    console.log('connected')
+}
+drone.on('connected', connected)
+drone.on('message', (message) => console.log('this is the message', message))
+
+
 router.get('/takeoff', (req,res,next) => {
-    res.json(client.takeoff())
+    client.takeoff()
+    res.json('taking off')
 })
 
 router.get('/land', (req,res,next) => {
-    res.json(client.land())
+    client.stop()
+    client.land()
+    res.json('landing')
 })
 
 router.get('/battery', (req,res,next) => {
+    const batt = client.battery()
+    console.log('battery', batt)
     res.json(client.battery())
+})
+
+router.get('/reset', (req,res,next) => {
+    client.resume()
+    res.json('resuming flight')
 })
 
 module.exports = router
